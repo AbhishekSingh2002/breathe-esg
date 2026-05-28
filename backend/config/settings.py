4444ca4604d2a5bcce6c6962cd116a59c76a29db
AbhishekSingh2002 <abhishek.singh@example.com>
2026-05-28 19:52:19 +0530

    Add missing dj_database_url import

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -2,6 +2,7 @@ import os
 from pathlib import Path
 from datetime import timedelta
 from decouple import config, Csv
+import dj_database_url
 
 # Build paths
 BASE_DIR = Path(__file__).resolve().parent.parent
